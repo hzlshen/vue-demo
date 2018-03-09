bb653bc6b04826194882f8f7ecc0865d2706a329
hzlshen <m15277968105@163.com>
2018-03-09 18:22:29 +0800

    ........

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ export default {
   data() {
     return {
       msg: "这是第一个vue cli",
-      show: true
+      show: true,
     };
     
   }
@@ -26,19 +26,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
